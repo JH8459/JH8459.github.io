4c70e658cf4f2501a894ff8f6aa3cea4179c8f1a
jh8459 <kk_ong2233@naver.com>
2025-06-27 12:38:01 +0900

    Modify: gatsby-meta-config.js 수정 (projects 순서 수정)

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -106,25 +106,14 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '📚 슬랙 사내 도서 리스트 채팅 봇',
+        title: '🤖 프로그래머스 프로필 뱃지 자동 생성 봇',
         description:
-          '웹 프로그래밍 개발이 점점 익숙해지며 실제로 "내가 필요한걸 직접 구현해 보는건 어떨까?"라는 생각은 점점 부풀려져서 업무하는 동안 자주 사용하는 슬랙 메신저와 노션을 활용해서 (주)ACG 사내 도서 리스트(노션DB 활용) 기능과 도서 대출 기록 기능을 제공하는 채팅 봇을 개발해보았습니다.',
-        techStack: ['NodeJS', 'Express', 'Slack API', 'Notion API'],
-        thumbnailUrl: 'project-slack.jpg',
+          '알고리즘 문제 풀이 동기부여를 위해 프로그래머스에서 제공하는 API를 활용하여 프로필 뱃지를 자동으로 생성해주는 봇을 개발하였습니다. Github Action을 활용하여 백준 허브에 연동된 저장소가 변경될 때 마다 호출되어 프로필 뱃지를 자동으로 업데이트 되도록 구성하였습니다.',
+        techStack: ['NodeJS', 'NestJS', 'Github', 'Github ACtion'],
+        thumbnailUrl: 'project-programmers.jpg',
         links: {
-          post: 'https://blog.jh8459.com/2023-05-01-PROJECT/',
-          github: 'https://github.com/JH8459/SLACK-LUNCH-BOT',
-        },
-      },
-      {
-        title: '💍 모바일 청첩장',
-        description:
-          '"개발자가 모바일 청첩장을 외부업체에 맡겨..??"라는 단순한 한가지 생각으로 시작하게된 모바일 청첩장 개발은 (백엔드 개발자 에겐) 쉽지 않았습니다. 이 또한 추억이라 생각하며 아내와 함께 기획부터 개발까지 완성하여 지인들에게 공유한 실제 모바일 청첩장 개발 히스토리를 소개합니다.',
-        techStack: ['React', 'Kakao API'],
-        thumbnailUrl: 'project-wedding.jpg',
-        links: {
-          github: 'https://github.com/JH8459/WEDDING-INVITATION-LETTER',
-          demo: 'https://wedding.jh8459.com/',
+          post: 'https://blog.jh8459.com/2024-12-22-PROJECT/',
+          github: 'https://github.com/JH8459/PROGRAMMERS-BADGE',
         },
       },
       {
@@ -140,16 +129,27 @@ module.exports = {
         },
       },
       {
-        title: '🤖 프로그래머스 프로필 뱃지 자동 생성 봇',
+        title: '💍 모바일 청첩장',
         description:
-          '알고리즘 문제 풀이 동기부여를 위해 프로그래머스에서 제공하는 API를 활용하여 프로필 뱃지를 자동으로 생성해주는 봇을 개발하였습니다. Github Action을 활용하여 백준 허브에 연동된 저장소가 변경될 때 마다 호출되어 프로필 뱃지를 자동으로 업데이트 되도록 구성하였습니다.',
-        techStack: ['NodeJS', 'NestJS', 'Github', 'Github ACtion'],
-        thumbnailUrl: 'project-programmers.jpg',
+          '"개발자가 모바일 청첩장을 외부업체에 맡겨..??"라는 단순한 한가지 생각으로 시작하게된 모바일 청첩장 개발은 (백엔드 개발자 에겐) 쉽지 않았습니다. 이 또한 추억이라 생각하며 아내와 함께 기획부터 개발까지 완성하여 지인들에게 공유한 실제 모바일 청첩장 개발 히스토리를 소개합니다.',
+        techStack: ['React', 'Kakao API'],
+        thumbnailUrl: 'project-wedding.jpg',
         links: {
-          post: 'https://blog.jh8459.com/2024-12-22-PROJECT/',
-          github: 'https://github.com/JH8459/PROGRAMMERS-BADGE',
+          github: 'https://github.com/JH8459/WEDDING-INVITATION-LETTER',
+          demo: 'https://wedding.jh8459.com/',
         },
       },
+      {
+        title: '📚 슬랙 사내 도서 리스트 채팅 봇',
+        description:
+          '웹 프로그래밍 개발이 점점 익숙해지며 실제로 "내가 필요한걸 직접 구현해 보는건 어떨까?"라는 생각은 점점 부풀려져서 업무하는 동안 자주 사용하는 슬랙 메신저와 노션을 활용해서 (주)ACG 사내 도서 리스트(노션DB 활용) 기능과 도서 대출 기록 기능을 제공하는 채팅 봇을 개발해보았습니다.',
+        techStack: ['NodeJS', 'Express', 'Slack API', 'Notion API'],
+        thumbnailUrl: 'project-slack.jpg',
+        links: {
+          post: 'https://blog.jh8459.com/2023-05-01-PROJECT/',
+          github: 'https://github.com/JH8459/SLACK-LUNCH-BOT',
+        },
+      }
     ],
   },
 };
