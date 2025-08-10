08e92b4bd5003e85c4a7a6f1a56e8e49135bf952
jh8459 <kk_ong2233@naver.com>
2025-08-10 19:33:58 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-73989fff4e3a69a3d54b.js"
+    "url": "webpack-runtime-d4396338e42820670e9a.js"
   },
   {
     "url": "framework-d319b3bf8c84750a438e.js"
   },
   {
-    "url": "styles.5af1b85930cb90e3ff85.css"
+    "url": "styles.cc21b585d3f91eff9431.css"
   },
   {
     "url": "app-cbcddf25e3be2b4d6d3a.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "294997fe79dca2c85bbe7d9199cb9647"
+    "revision": "286733f181ef8f5b31336424cb108090"
   },
   {
     "url": "manifest.webmanifest",
